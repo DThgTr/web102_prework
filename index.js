752a3424a7767e0d8bf69e9cdbea466c5bb71d7d
DThgTr <121519286+DThgTr@users.noreply.github.com>
2024-01-12 17:49:26 -0600

    Challenge 4 completed. Everything worked now

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-    for (i = 0; i < games.length; i++) {
+    for (var i = 0; i < games.length; i++) {
         // create a new div element, which will become the game card
         const gameCard = document.createElement("div");
         // add the class game-card to the list
@@ -65,16 +65,21 @@ addGamesToPage(GAMES_JSON);
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const backersTotal = GAMES_JSON.reduce((acc, game) => {
+                        return acc + game.backers;
+                    }, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `${backersTotal.toLocaleString('en-US')}`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
 
+const totalRaised = GAMES_JSON.reduce((acc, game) => {
+                                    return acc + game.pledged;
+                                }, 0);
 // set inner HTML using template literal
-
+raisedCard.innerHTML = `${totalRaised.toLocaleString('en-US')}`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
